d580fd7de4d12e28f72519d565ad14faa51677de
UTKARSH <86353517+dhecode@users.noreply.github.com>
2023-07-18 04:04:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, function () {
+  console.log("Server Insitiated");
+});
+
+app.get("/style.css", function (req, res) {
+  res.sendFile(__dirname + "/" + "style.css");
+});
+
+app.get("/script.js", function (req, res) {
+  res.sendFile(__dirname + "/" + "script.js");
+});
